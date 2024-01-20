a355fecf3181db726f6d89df3d402fce77c84eb7
LiuZhan <lz2915253393@163.com>
2024-01-21 01:44:42 +0800

    添加 SCharacter class

---
+++ ActionRoguelike/Source/ActionRoguelike/Public/SCharacter.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+
+//前置声明
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+
+//函数
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float value);
+	void MoveRight(float value);
+
+
+//属性
+protected:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	//移动
+	UPROPERTY(EditAnywhere);
+	float MoveSpeed;
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
